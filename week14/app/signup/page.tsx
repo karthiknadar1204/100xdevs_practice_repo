d4bc52cc7f9f97ea1402ea1668cb19d070a42f43
karthik <karthiknadar1204@gmail.com>
2024-07-04 16:49:10 +0530

    practice

---
+++ week14/app/signup/page.tsx
@@ -0,0 +1,14 @@
+
+import { Signup } from '@/components/Signup'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Signup/>
+
+    </div>
+  )
+}
+
+export default page
